1ef1fd6b2cb4980ff30b02f3483486e530b6ca1d
Kartik Verma <kvs1297@gmail.com>
2019-04-06 22:36:51 +0530

    added home page

--- Jam/urls.py
+++ Jam/urls.py
@@ -21,6 +21,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', TemplateView.as_view(template_name = 'home.html'), name="main_home"),
     path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('user/', include('Essentials.urls')),
