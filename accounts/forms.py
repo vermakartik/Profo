17659fcb827fec947ba3109e43f65c890255a335
jayantjangir <jayantjangir97@gmail.com>
2019-04-14 13:32:37 +0530

    added email verification

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,8 +1,15 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, PasswordResetForm
+from django import forms
+from django.contrib.auth.models import User
 
 class CustomUserCreationForm(UserCreationForm):
 
     def __init__(self, *args, **kwargs):
         kwargs.setdefault('label_suffix', "")
         super(CustomUserCreationForm, self).__init__(*args, **kwargs)
-        
+
+class SignUpForm(UserCreationForm):
+    email = forms.EmailField(max_length=256, required=True)
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
